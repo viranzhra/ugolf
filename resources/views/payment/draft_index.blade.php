25b27585e860967c8a5c17e70dce9114e724284d
rayys0703 <rayys0703@gmail.com>
2024-11-14 15:13:48 +0700

    Memperbarui route web

--- resources/views/payment/draft_index.blade.php
+++ resources/views/payment/draft_index.blade.php
@@ -57,7 +57,7 @@
         </div>
 
         <div class="d-grid gap-2 btn-container" style="position: relative;bottom: 0;right: 0;">
-            <button id="check-status-btn" class="mt-3"><svg id="svgBack" style="display:none" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fcfcfc" viewBox="0 0 256 256"><path d="M224,88v80a16,16,0,0,1-16,16H128v40a8,8,0,0,1-13.66,5.66l-96-96a8,8,0,0,1,0-11.32l96-96A8,8,0,0,1,128,32V72h80A16,16,0,0,1,224,88Z"></path></svg><b id="name-status-btn">Cek Status Transaksi</b></button>
+            <button id="check-status-btn" class="mt-3 d-flex align-items-start"><svg id="svgBack" style="display:none" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fcfcfc" viewBox="0 0 256 256"><path d="M224,88v80a16,16,0,0,1-16,16H128v40a8,8,0,0,1-13.66,5.66l-96-96a8,8,0,0,1,0-11.32l96-96A8,8,0,0,1,128,32V72h80A16,16,0,0,1,224,88Z"></path></svg><b id="name-status-btn">Cek Status Transaksi</b></button>
         </div>
 
     </div>       
